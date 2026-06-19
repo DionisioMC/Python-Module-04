008fb88bdab20df01edbf33da8a7685450a4896e
Dionisio Coelho <dcoelho@c1r10s4.42porto.com>
2026-06-19 16:51:35 +0100

    Minor bug fixes

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -16,22 +16,23 @@ if __name__ == "__main__":
             print("---")
             file.close()
             print(f"File '{args[1]}' closed.")
+            print("Transform data:\n")
+            print("---\n")
+            new_text = text.replace("\n", "#\n")
+            print(new_text)
+            print("---")
+            new_file_name = input("Enter new file name (or empty): ").strip()
+            if new_file_name == "":
+                print("Not saving data.\n")
+            else:
+                try:
+                    print(f"Saving data to '{new_file_name}'")
+                    new_file = open(new_file_name, "w")
+                    new_file.write(new_text)
+                    new_file.close()
+                    print(f"Data saved in file '{new_file_name}'.\n")
+                except PermissionError as e:
+                    print("Error opening/writing in file "
+                          f"'{new_file_name}': {e}\n")
         except Exception as e:
             print(f"Error opening file '{args[1]}': {e}\n")
-        print("Transform data:\n")
-        print("---\n")
-        new_text = text.replace("\n", "#\n")
-        print(new_text)
-        print("---")
-        new_file_name = input("Enter new file name (or empty): ")
-        if new_file_name == "":
-            print("Not saving data.\n")
-        else:
-            try:
-                print(f"Saving data to '{new_file_name}'")
-                new_file = open(new_file_name, "w")
-                new_file.write(new_text)
-                new_file.close()
-                print(f"Data saved in file '{new_file_name}'.\n")
-            except PermissionError as e:
-                print(f"Error opening file '{new_file_name}': {e}\n")
